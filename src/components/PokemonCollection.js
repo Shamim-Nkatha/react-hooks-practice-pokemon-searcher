7f3ea077bfbe9f81df3c7977f5609ac103995c80
Shamim-Nkatha <123588376+Shamim-Nkatha@users.noreply.github.com>
2023-02-28 23:22:38 +0300

    Add files via upload

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -2,10 +2,15 @@ import React from "react";
 import PokemonCard from "./PokemonCard";
 import { Card } from "semantic-ui-react";
 
-function PokemonCollection() {
+function PokemonCollection({ pokemons }) {
+  
+  const renderPokemons = pokemons.map(pokemonObj => (
+    <PokemonCard key={pokemonObj.id} pokemon={pokemonObj} />
+  ))
+
   return (
     <Card.Group itemsPerRow={6}>
-      <h1>Hello From Pokemon Collection</h1>
+      { renderPokemons }
     </Card.Group>
   );
 }
--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -1,19 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PokemonCollection from "./PokemonCollection";
 import PokemonForm from "./PokemonForm";
 import Search from "./Search";
 import { Container } from "semantic-ui-react";
 
 function PokemonPage() {
+
+  const [pokemons,setPokemons] = useState([])
+  const [backupList, setBackList] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:3001/pokemon')
+    .then(res => res.json())
+    .then(data => {
+      setPokemons(data)
+      setBackList(data)
+    })
+  },[])
+
+  const addNewPokemon = (pokemonObj) => {
+    setPokemons([ ...pokemons, pokemonObj ])
+  }
+
+  const searchPokemon = (searchText) => {
+    // console.log(searchText)
+    const filteredPokemons = backupList.filter(pokemon => {
+      return pokemon.name.includes(searchText)
+    })
+    setPokemons(filteredPokemons)
+  }
+
   return (
     <Container>
       <h1>Pokemon Searcher</h1>
       <br />
-      <PokemonForm />
+      <PokemonForm addNewPokemon={addNewPokemon} />
       <br />
-      <Search />
+      <Search searchPokemon={searchPokemon} />
       <br />
-      <PokemonCollection />
+      <PokemonCollection pokemons={pokemons} />
     </Container>
   );
 }
